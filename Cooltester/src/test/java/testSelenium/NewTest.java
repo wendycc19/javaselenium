35dcd8dab5b2d16d8ec239449dd943de6f70af9a
wendy <wendy@OmiPC>
2023-10-06 18:49:13 -0600

    Primer commit

---
+++ Cooltester/src/test/java/testSelenium/NewTest.java
@@ -0,0 +1,13 @@
+package testSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+  }
+}
